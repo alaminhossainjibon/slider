6af37dc9dd49e18d7945e384957e3b09cbd77fbd
alaminhossainjibon <mdalamin7622@gmail.com>
2022-03-12 11:37:35 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const images = [
+    './images/pic1.jpg',
+    './images/pic2.jpg',
+    './images/pic3.jpg',
+    './images/pic4.jpg',
+    './images/pic5.jpg',
+    './images/pic6.jpg',
+    './images/pic7.jpg',
+    './images/pic8.jpg',
+    './images/pic9.jpg',
+];
+let imgIndex = 0;
+const imgElement = document.getElementById('slider-img');
+setInterval(() => {
+    if (imgIndex >= images.length) {
+        imgIndex = 0;
+    }
+    const imgUrl = images[imgIndex];
+    imgElement.setAttribute('src', imgUrl);
+    imgIndex++;
+}, 1000)
